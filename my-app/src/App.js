c35bbf3d24fb3ac1c4e0bf1bea36c9b8ac422e92
braydenNP <S10258659@connect.np.edu.sg>
2024-01-16 08:17:34 +0800

    changed homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 function App() {
   return (
     <div className="App">
